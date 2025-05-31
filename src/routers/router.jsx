5c9aafa07fb69fbeb20df4415319e6e5bb4c8348
Roni Biswas <ronibiswas068@gmail.com>
2025-06-01 00:31:07 +0600

    modified mainlayout and route setup and display navbar, footer and dynamic content

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
   },
 ]);
 
